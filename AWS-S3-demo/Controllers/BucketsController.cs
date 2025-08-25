112b548ea9ab346a6ddb3e80734296357e070921
prakash <prakashko@examroom.ai>
2025-08-25 19:28:20 +0530

    Add project files.

---
+++ AWS-S3-demo/Controllers/BucketsController.cs
@@ -0,0 +1,53 @@
+﻿using Amazon.S3;
+using Amazon.S3.Util;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWS_S3_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BucketsController : ControllerBase
+    {
+        private readonly IAmazonS3 _s3Client;
+
+        public BucketsController(IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllBucketAsync()
+        {
+            var data = await _s3Client.ListBucketsAsync();
+            var buckets = data.Buckets.Select(b =>
+            {
+                return b.BucketName;
+            });
+            return Ok(buckets);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateBucketAsync(string bucketName)
+        {
+            var bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (bucketExists)
+            {
+                return BadRequest($"Bucket {bucketName} already exists");
+            }
+            await _s3Client.PutBucketAsync(bucketName);
+            return Ok($"Bucket {bucketName} created successfully.");
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteBucketAsync(string bucketName)
+        {
+            var bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists)
+            {
+                return BadRequest($"Bucket {bucketName} does not exists");
+            }
+            await _s3Client.DeleteBucketAsync(bucketName);
+            return NoContent();
+        }
+    }
+}
---
+++ AWS-S3-demo/Models/S3ObjectDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AWS_S3_demo.Models
+{
+    public class S3ObjectDto
+    {
+        public string? Name { get; set; }
+        public string? PresignedUrl { get; set; }
+    }
+}
